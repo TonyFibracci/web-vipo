024e818f82516414b5814b73197f57cc3b7bdc1e
Suiteng <suiteng88@gmail.com>
2018-10-11 11:26:05 +0200

    Initial project import

---
+++ web-vipo/src/main/java/ey/faasquant/webvipo/WebVipoApplication.java
@@ -0,0 +1,12 @@
+package ey.faasquant.webvipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebVipoApplication.class, args);
+	}
+}
---
+++ web-vipo/src/test/java/ey/faasquant/webvipo/WebVipoApplicationTests.java
@@ -0,0 +1,16 @@
+package ey.faasquant.webvipo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebVipoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
